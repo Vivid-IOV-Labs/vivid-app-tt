3b996b9af2d7bd95e7249aa2c3db974bf4c7b801
bolladaniele <bolladaniele@gmail.com>
2020-11-10 14:57:31 +0000

    npm update, lint

--- src/util/constants/tippingContract.js
+++ src/util/constants/tippingContract.js
@@ -1,54 +1,54 @@
-const address = "0x6915Ae0D5a9c08675Ce5c83b54228f3D3F1A6101"
+const address = "0x6915Ae0D5a9c08675Ce5c83b54228f3D3F1A6101";
 const ABI = [
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "name": "_addr",
-                "type": "address"
-            }
-        ],
-        "name": "tip",
-        "outputs": [],
-        "payable": true,
-        "stateMutability": "payable",
-        "type": "function"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "name": "from",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "name": "to",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "name": "value",
-                "type": "uint256"
-            }
-        ],
-        "name": "Tip",
-        "type": "event"
-    },
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "amount",
-        "outputs": [
-            {
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    }
-]
-export { address, ABI }
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_addr",
+        type: "address"
+      }
+    ],
+    name: "tip",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: "from",
+        type: "address"
+      },
+      {
+        indexed: true,
+        name: "to",
+        type: "address"
+      },
+      {
+        indexed: false,
+        name: "value",
+        type: "uint256"
+      }
+    ],
+    name: "Tip",
+    type: "event"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "amount",
+    outputs: [
+      {
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  }
+];
+export { address, ABI };
--- src/util/getWeb3.js
+++ src/util/getWeb3.js
@@ -1,104 +1,103 @@
-import Web3 from 'web3'
+import Web3 from "web3";
 
 /*
-* 1. Check for injected web3 (mist/metamask)
-* 2. If metamask/mist create a new web3 instance and pass on result
-* 3. Get networkId - Now we can check the user is connected to the right network to use our dApp
-* 4. Get user account from metamask
-* 5. Get user balance
-*/
+ * 1. Check for injected web3 (mist/metamask)
+ * 2. If metamask/mist create a new web3 instance and pass on result
+ * 3. Get networkId - Now we can check the user is connected to the right network to use our dApp
+ * 4. Get user account from metamask
+ * 5. Get user balance
+ */
 
-let getWeb3 = new Promise(function (resolve, reject) {
-    // Check for injected web3 (mist/metamask)
-    var web3js = window.web3
-    if (typeof web3js !== 'undefined') {
-        console.log(web3js.currentProvider)
+let getWeb3 = new Promise(function(resolve, reject) {
+  // Check for injected web3 (mist/metamask)
+  var web3js = window.web3;
+  if (typeof web3js !== "undefined") {
+    console.log(web3js.currentProvider);
 
-        try {
-            let ethereum = window.ethereum;
-            // Request account access if needed
-            ethereum.enable().then(
-                () => {
-                    resolve({
-                        web3js
-                    })
-                }
-            )
-
-        } catch (error) {
-            // User denied account access...
-            console.log('ethereum did not enable')
-        }
-
-    } else {
-        // web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')) GANACHE FALLBACK
-        reject(new Error('Unable to connect to Metamask'))
+    try {
+      let ethereum = window.ethereum;
+      // Request account access if needed
+      ethereum.enable().then(() => {
+        resolve({
+          web3js
+        });
+      });
+    } catch (error) {
+      // User denied account access...
+      console.log("ethereum did not enable");
     }
+  } else {
+    // web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')) GANACHE FALLBACK
+    reject(new Error("Unable to connect to Metamask"));
+  }
 })
-    .then(result => {
-        return new Promise(function (resolve, reject) {
-            var web3 = new Web3(result.web3js.currentProvider)
-            console.log(web3)
+  .then(result => {
+    return new Promise(function(resolve, reject) {
+      var web3 = new Web3(result.web3js.currentProvider);
+      console.log(web3);
 
-            if (!web3) {
-                reject(new Error('Unable to create new Web3 instance'))
-            } else {
+      if (!web3) {
+        reject(new Error("Unable to create new Web3 instance"));
+      } else {
+        result = Object.assign({}, result, {
+          injectedWeb3: web3.isConnected()
+        });
+        result = Object.assign({}, result, {
+          web3() {
+            return web3;
+          }
+        });
 
-                result = Object.assign({}, result, { injectedWeb3: web3.isConnected() })
-                result = Object.assign({}, result, {
-                    web3() {
-                        return web3
-                    }
-                })
+        resolve(result);
+      }
+    });
+  })
+  .then(result => {
+    return new Promise(function(resolve, reject) {
+      // Retrieve network ID
+      result.web3().version.getNetwork((err, networkId) => {
+        if (err) {
+          // If we can't find a networkId keep result the same and reject the promise
+          reject(new Error("Unable to retrieve network ID"));
+        } else {
+          // Assign the networkId property to our result and resolve promise
+          result = Object.assign({}, result, { networkId });
+          resolve(result);
+        }
+      });
+    });
+  })
+  .then(result => {
+    return new Promise(function(resolve, reject) {
+      // Retrieve coinbase
+      result.web3().eth.getCoinbase((err, coinbase) => {
+        console.log(coinbase);
+        if (err) {
+          reject(new Error("Unable to retrieve coinbase"));
+        } else {
+          result = Object.assign({}, result, { coinbase });
+          resolve(result);
+        }
+      });
+    });
+  })
+  .then(result => {
+    return new Promise(function(resolve, reject) {
+      // Retrieve balance for coinbase
+      console.log(result);
+      result.web3().eth.getBalance(result.coinbase, (err, balance) => {
+        if (err) {
+          reject(
+            new Error(
+              "Unable to retrieve balance for address: " + result.coinbase
+            )
+          );
+        } else {
+          result = Object.assign({}, result, { balance });
+          resolve(result);
+        }
+      });
+    });
+  });
 
-                resolve(result)
-
-
-            }
-        })
-    })
-    .then(result => {
-        return new Promise(function (resolve, reject) {
-            // Retrieve network ID
-            result.web3().version.getNetwork((err, networkId) => {
-                if (err) {
-                    // If we can't find a networkId keep result the same and reject the promise
-                    reject(new Error('Unable to retrieve network ID'))
-                } else {
-                    // Assign the networkId property to our result and resolve promise
-                    result = Object.assign({}, result, { networkId })
-                    resolve(result)
-                }
-            })
-        })
-    })
-    .then(result => {
-        return new Promise(function (resolve, reject) {
-            // Retrieve coinbase
-            result.web3().eth.getCoinbase((err, coinbase) => {
-                console.log(coinbase)
-                if (err) {
-                    reject(new Error('Unable to retrieve coinbase'))
-                } else {
-                    result = Object.assign({}, result, { coinbase })
-                    resolve(result)
-                }
-            })
-        })
-    })
-    .then(result => {
-        return new Promise(function (resolve, reject) {
-            // Retrieve balance for coinbase
-            console.log(result)
-            result.web3().eth.getBalance(result.coinbase, (err, balance) => {
-                if (err) {
-                    reject(new Error('Unable to retrieve balance for address: ' + result.coinbase))
-                } else {
-                    result = Object.assign({}, result, { balance })
-                    resolve(result)
-                }
-            })
-        })
-    })
-
-export default getWeb3
+export default getWeb3;
