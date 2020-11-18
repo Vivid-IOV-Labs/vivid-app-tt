d7205e42615e18720f647885965454749844d6e7
bolladaniele <bolladaniele@gmail.com>
2020-11-18 13:47:27 +0000

    scroller on req dialog

--- src/components/dialogs/RequestDialog.vue
+++ src/components/dialogs/RequestDialog.vue
@@ -9,74 +9,76 @@
           </v-ons-button>
         </div>
       </v-ons-toolbar>
-      <v-ons-list>
-        <v-ons-list-item>
-          <div class="flex-coulumn p-4">
-            <textarea
-              title="Request Title"
-              class="textarea textarea--transparent"
-              placeholder="What do you want to see?"
-              v-model="requestModel.mapPin.details"
-            >
-            </textarea>
-            <small>[10 words max]</small>
-          </div>
-        </v-ons-list-item>
-        <v-ons-list-item>
-          <div class="flex-coulumn p-4">
-            <geo-search-bar
-              id="request_location"
-              title="Search"
-              placeholder="Add Location"
-              @change="onSelectAddress"
-            ></geo-search-bar>
-          </div>
-        </v-ons-list-item>
-        <v-ons-list-item>
-          <div class="flex-coulumn p-4">
-            <v-ons-list>
-              <div>
-                <v-ons-list-item>
-                  <span>#</span
-                  ><v-ons-input
-                    title="Add Hashtags"
-                    v-model="requestModel.mapPin.twitterHashTags[0]"
-                    placeholder="Add Hashtag"
-                    float
-                  >
-                  </v-ons-input>
-                </v-ons-list-item>
-                <v-ons-list-item>
-                  <span>#</span
-                  ><v-ons-input
-                    title="Add Hashtags"
-                    v-model="requestModel.mapPin.twitterHashTags[1]"
-                    placeholder="Add Hashtag "
-                  >
-                  </v-ons-input>
-                </v-ons-list-item>
-                <v-ons-list-item>
-                  <span>#</span
-                  ><v-ons-input
-                    title="Add Hashtags"
-                    v-model="requestModel.mapPin.twitterHashTags[2]"
-                    placeholder="Add Hashtag "
-                  >
-                  </v-ons-input>
-                </v-ons-list-item>
-              </div>
-            </v-ons-list>
-          </div>
-        </v-ons-list-item>
-      </v-ons-list>
-      <div class="flex-center-xy p-2">
-        <v-ons-button
-          class="btn btn--request btn--full btn--large"
-          @click="closeRequestDialog"
-        >
-          Confirm
-          <base-icon name="request"></base-icon>
-        </v-ons-button>
+      <div class="scroller">
+        <v-ons-list>
+          <v-ons-list-item>
+            <div class="flex-coulumn p-4">
+              <textarea
+                title="Request Title"
+                class="textarea textarea--transparent"
+                placeholder="What do you want to see?"
+                v-model="requestModel.mapPin.details"
+              >
+              </textarea>
+              <small>[10 words max]</small>
+            </div>
+          </v-ons-list-item>
+          <v-ons-list-item>
+            <div class="flex-coulumn p-4">
+              <geo-search-bar
+                id="request_location"
+                title="Search"
+                placeholder="Add Location"
+                @change="onSelectAddress"
+              ></geo-search-bar>
+            </div>
+          </v-ons-list-item>
+          <v-ons-list-item>
+            <div class="flex-coulumn p-4">
+              <v-ons-list>
+                <div>
+                  <v-ons-list-item>
+                    <span>#</span
+                    ><v-ons-input
+                      title="Add Hashtags"
+                      v-model="requestModel.mapPin.twitterHashTags[0]"
+                      placeholder="Add Hashtag"
+                      float
+                    >
+                    </v-ons-input>
+                  </v-ons-list-item>
+                  <v-ons-list-item>
+                    <span>#</span
+                    ><v-ons-input
+                      title="Add Hashtags"
+                      v-model="requestModel.mapPin.twitterHashTags[1]"
+                      placeholder="Add Hashtag "
+                    >
+                    </v-ons-input>
+                  </v-ons-list-item>
+                  <v-ons-list-item>
+                    <span>#</span
+                    ><v-ons-input
+                      title="Add Hashtags"
+                      v-model="requestModel.mapPin.twitterHashTags[2]"
+                      placeholder="Add Hashtag "
+                    >
+                    </v-ons-input>
+                  </v-ons-list-item>
+                </div>
+              </v-ons-list>
+            </div>
+          </v-ons-list-item>
+        </v-ons-list>
+        <div class="flex-center-xy p-2">
+          <v-ons-button
+            class="btn btn--request btn--full btn--large"
+            @click="closeRequestDialog"
+          >
+            Confirm
+            <base-icon name="request"></base-icon>
+          </v-ons-button>
+        </div>
       </div>
     </v-ons-page>
   </v-ons-dialog>
