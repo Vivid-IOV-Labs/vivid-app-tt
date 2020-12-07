5e3cf4e44ebe4eae3b42397f123b504c7ff386e1
bolladaniele <bolladaniele@gmail.com>
2020-12-07 16:58:56 +0000

    add mixapenl

--- src/util/analytics.js
+++ src/util/analytics.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-undef */
 export const trackPage = path => {
   ga("send", "pageview", path);
+  mixpanel.track(`Page view: ${path}`);
 };
 
 export const trackEvent = ({ category, action, label = null }) => {
   ga("send", "event", category, action, label);
+  mixpanel.track(`${category} - ${action}`);
 };
