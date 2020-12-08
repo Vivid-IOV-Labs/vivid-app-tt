98e63e9f8064760e7cd3fba941ca8f939dccabb0
bolladaniele <bolladaniele@gmail.com>
2020-12-08 15:51:42 +0000

    add getSmartContract

---
+++ src/util/getSmartContract.js
@@ -0,0 +1,17 @@
+import {
+  address as smartContractAddress,
+  ABI as smartContractABI
+} from "@/util/constants/tippingContract";
+import { ethers } from "ethers";
+
+const getSmartContract = async ({ web3, signer }) => {
+  const smartContractProvider = web3();
+  const tippingContract = await new ethers.Contract(
+    smartContractAddress,
+    smartContractABI,
+    smartContractProvider
+  );
+  const tippingContractWithSigner = await tippingContract.connect(signer);
+  return tippingContractWithSigner;
+};
+export default getSmartContract;
