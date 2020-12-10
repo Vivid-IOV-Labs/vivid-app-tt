1c8890b9e77f0f5db02533109e5b5a0f709a8f0e
bolladaniele <bolladaniele@gmail.com>
2020-12-10 16:35:16 +0000

    add selection

--- src/components/dialogs/ContentFeedDialog.vue
+++ src/components/dialogs/ContentFeedDialog.vue
@@ -13,84 +13,184 @@
         <h3>What what do you like to see most?</h3>
         <div class="grid-x3">
           <div class="grid-x3__cell flex-column flex-center-xy  mb-2 ">
-            <v-ons-button
-              class="btn btn--round-large text-azure btn--opacity-soft nd mb-2"
+            <label
+              for="crypto_content"
+              class="mb-2 text-center flex-column flex-center-xy "
             >
-              <base-icon class="btn__icon" name="crypto"></base-icon>
-            </v-ons-button>
-            <span class="mb-2 text-center">Crypto</span>
+              <input
+                type="checkbox"
+                style="display:none"
+                value="crypto"
+                v-model="contentSelected"
+                id="crypto_content"
+              />
+
+              <v-ons-button
+                class="btn btn--checkbox btn--round-large text-azure btn--opacity-soft nd mb-2"
+              >
+                <base-icon class="btn__icon" name="crypto"></base-icon>
+              </v-ons-button>
+              Crypto</label
+            >
           </div>
           <div class="grid-x3__cell flex-column flex-center-xy  mb-2">
-            <v-ons-button
-              class="btn btn--round-large text-azure btn--opacity-soft nd mb-2"
+            <label
+              for="gaming_content"
+              class="mb-2 text-center flex-column flex-center-xy "
             >
-              <base-icon
-                class="btn__icon"
-                :fill="false"
-                name="gaming"
-              ></base-icon>
-            </v-ons-button>
-            <span class="mb-2 text-center">Gaming</span>
+              <input
+                type="checkbox"
+                style="display:none"
+                value="gaming"
+                v-model="contentSelected"
+                id="gaming_content"
+              />
+              <v-ons-button
+                class="btn btn--checkbox btn--round-large text-azure btn--opacity-soft nd mb-2"
+              >
+                <base-icon
+                  class="btn__icon"
+                  :fill="false"
+                  name="gaming"
+                ></base-icon> </v-ons-button
+              >Gaming</label
+            >
           </div>
           <div class="grid-x3__cell flex-column flex-center-xy  mb-2">
-            <v-ons-button
-              class="btn btn--round-large text-azure btn--opacity-soft nd mb-2"
+            <label
+              for="sports_content"
+              class="mb-2 text-center flex-column flex-center-xy "
             >
-              <base-icon class="btn__icon" name="sports"></base-icon>
-            </v-ons-button>
-            <span class="mb-2 text-center">Sports</span>
+              <input
+                type="checkbox"
+                style="display:none"
+                value="sports"
+                v-model="contentSelected"
+                id="sports_content"
+              />
+              <v-ons-button
+                class="btn btn--checkbox btn--round-large text-azure btn--opacity-soft nd mb-2"
+              >
+                <base-icon
+                  class="btn__icon"
+                  name="sports"
+                ></base-icon> </v-ons-button
+              >Sports</label
+            >
           </div>
           <div class="grid-x3__cell flex-column flex-center-xy  mb-2">
-            <v-ons-button
-              class="btn btn--round-large text-azure btn--opacity-soft nd mb-2"
+            <label
+              for="entertainment_content"
+              class="mb-2 text-center flex-column flex-center-xy "
             >
-              <base-icon class="btn__icon" name="entertainment"></base-icon>
-            </v-ons-button>
-            <span class="mb-2 text-center">Entertainment</span>
+              <input
+                type="checkbox"
+                style="display:none"
+                value="entertainment"
+                v-model="contentSelected"
+                id="entertainment_content"
+              />
+              <v-ons-button
+                class="btn btn--checkbox btn--round-large text-azure btn--opacity-soft nd mb-2"
+              >
+                <base-icon
+                  class="btn__icon"
+                  name="entertainment"
+                ></base-icon> </v-ons-button
+              >Entertainment</label
+            >
           </div>
           <div class="grid-x3__cell flex-column flex-center-xy  mb-2">
-            <v-ons-button
-              class="btn btn--round-large text-azure btn--opacity-soft mb-2"
+            <label
+              for="food_drinks_content"
+              class="mb-2 text-center flex-column flex-center-xy "
             >
-              <base-icon
-                class="btn__icon"
-                :fill="false"
-                name="food_drink"
-              ></base-icon>
-            </v-ons-button>
-            <span class="mb-2 text-center">Food &amp; Drink</span>
+              <input
+                type="checkbox"
+                style="display:none"
+                value="food_drinks"
+                v-model="contentSelected"
+                id="food_drinks_content"
+              />
+              <v-ons-button
+                class="btn btn--checkbox btn--round-large text-azure btn--opacity-soft mb-2"
+              >
+                <base-icon
+                  class="btn__icon"
+                  :fill="false"
+                  name="food_drink"
+                ></base-icon>
+              </v-ons-button>
+              Food &amp; Drink</label
+            >
           </div>
           <div class="grid-x3__cell flex-column flex-center-xy  mb-2">
-            <v-ons-button
-              class="btn btn--round-large text-azure btn--opacity-soft mb-2"
+            <label
+              for="healt_wellbeing_content"
+              class="mb-2 text-center flex-column flex-center-xy "
             >
-              <base-icon
-                class="btn__icon"
-                :fill="false"
-                name="healt_wellbeing"
-              ></base-icon>
-            </v-ons-button>
-            <span class="mb-2 text-center">Healt &amp; Wellbeing</span>
+              <input
+                type="checkbox"
+                style="display:none"
+                value="healt_wellbeing"
+                v-model="contentSelected"
+                id="healt_wellbeing_content"
+              />
+              <v-ons-button
+                class="btn btn--checkbox btn--round-large text-azure btn--opacity-soft mb-2"
+              >
+                <base-icon
+                  class="btn__icon"
+                  :fill="false"
+                  name="healt_wellbeing"
+                ></base-icon> </v-ons-button
+              >Healt &amp; Wellbeing</label
+            >
           </div>
           <div class="grid-x3__cell flex-column flex-center-xy  mb-2">
-            <v-ons-button
-              class="btn btn--round-large text-azure btn--opacity-soft mb-2"
+            <label
+              for="travel_outdoors_content"
+              class="mb-2 text-center flex-column flex-center-xy "
             >
-              <base-icon
-                class="btn__icon"
-                :fill="false"
-                name="travel_outdoors"
-              ></base-icon>
-            </v-ons-button>
-            <span class="mb-2 text-center">Travel &amp; Outdoors</span>
+              <input
+                type="checkbox"
+                style="display:none"
+                value="travel_outdoors"
+                v-model="contentSelected"
+                id="travel_outdoors_content"
+              />
+              <v-ons-button
+                class="btn btn--checkbox btn--round-large text-azure btn--opacity-soft mb-2"
+              >
+                <base-icon
+                  class="btn__icon"
+                  :fill="false"
+                  name="travel_outdoors"
+                ></base-icon> </v-ons-button
+              >Travel &amp; Outdoors</label
+            >
           </div>
           <div class="grid-x3__cell flex-column flex-center-xy  mb-2">
-            <v-ons-button
-              class="btn btn--round-large text-azure btn--opacity-soft mb-2"
+            <label
+              for="science_tech_content"
+              class="mb-2 text-center flex-column flex-center-xy "
             >
-              <base-icon class="btn__icon" name="science_tech"></base-icon>
-            </v-ons-button>
-            <span class="mb-2 text-center">Science &amp; Tech</span>
+              <input
+                type="checkbox"
+                style="display:none"
+                value="science_tech"
+                v-model="contentSelected"
+                id="science_tech_content"
+              />
+              <v-ons-button
+                class="btn btn--checkbox btn--round-large text-azure btn--opacity-soft mb-2"
+              >
+                <base-icon
+                  class="btn__icon"
+                  name="science_tech"
+                ></base-icon> </v-ons-button
+              >Science &amp; Tech</label
+            >
           </div>
         </div>
       </div>
@@ -101,6 +201,11 @@
 <script>
 export default {
   name: "ContentFeedDialog",
+  data() {
+    return {
+      contentSelected: []
+    };
+  },
   props: {
     value: {
       type: Boolean,
@@ -145,5 +250,17 @@ export default {
     margin-bottom: 1rem;
     flex: 0 1 25%;
   }
+}
+input[type="checkbox"]:checked ~ .btn--checkbox {
+  border: 2px solid $azure;
+}
+.btn--checkbox:hover {
+  background: $dark-grey;
+  color: $azure;
+}
+.btn--checkbox {
+  background: $dark-grey;
+  color: $azure;
+  border: 2px solid $dark-grey;
 }
 </style>
