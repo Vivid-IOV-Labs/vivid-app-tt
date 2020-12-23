78e0944bbf307d1a184956458787f20b17b976a6
bolladaniele <bolladaniele@gmail.com>
2020-12-23 11:59:13 +0000

    delete by code

--- src/store/modules/media.js
+++ src/store/modules/media.js
@@ -77,16 +77,14 @@ const mutations = {
   addHighlighted(state, item) {
     state.highlighted = [item, ...state.highlighted];
   },
-  delete(state, { mediaID }) {
-    state.all = state.all.filter(media => media.mediaID !== mediaID);
+  delete(state, { code }) {
+    state.all = state.all.filter(media => media.code !== code);
   },
-  deleteLatest(state, { mediaID }) {
-    state.latests = state.latests.filter(media => media.mediaID !== mediaID);
+  deleteLatest(state, { code }) {
+    state.latests = state.latests.filter(media => media.code !== code);
   },
-  deleteHighlighted(state, { mediaID }) {
-    state.highlighted = state.highlighted.filter(
-      media => media.mediaID !== mediaID
-    );
+  deleteHighlighted(state, { code }) {
+    state.highlighted = state.highlighted.filter(media => media.code !== code);
   },
   setTotalTip(state, { mediaID, totalTips }) {
     const mediaIndex = state.all.findIndex(media => media.mediaID === mediaID);
