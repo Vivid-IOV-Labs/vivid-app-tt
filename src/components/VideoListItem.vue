08a1b6a7d3741ae34fbef31bab90f93c83016b7e
bolladaniele <bolladaniele@gmail.com>
2021-03-16 13:11:05 +0000

    add border radiu

---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex-center-y full-width medialist__item ">
+    <img
+      class="medialist__item_poster"
+      :src="posterUrl"
+      :alt="media.details.title"
+    />
+    <div class="px-2 medialist__item__title">
+      {{ media.details.title }}
+    </div>
+    <base-icon class="ml-auto" name="angle-right"></base-icon>
+  </div>
+</template>
+
+<script>
+import BaseIcon from "@/components/BaseIcon.vue";
+import env from "@/env.js";
+
+export default {
+  name: "VideoList",
+  props: {
+    media: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    BaseIcon
+  },
+  computed: {
+    posterUrl() {
+      const url = `${env.media_storage}/${this.media.mediaID}.png`;
+      return url;
+    }
+  }
+};
+</script>
+<style lang="scss">
+.medialist__item {
+  position: relative;
+}
+.medialist__item_poster {
+  object-fit: contain;
+  width: 100%;
+  border-radius: 0.6rem;
+}
+.medialist__item__title {
+  position: absolute;
+  bottom: 2rem;
+  font-size: -8.8rem;
+  padding: 0.6rem 1rem;
+  background: rgb(0, 0, 0, 0.6);
+}
+</style>
