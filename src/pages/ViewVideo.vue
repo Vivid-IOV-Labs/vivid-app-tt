d573beb2562f8be507e3cfa87e6f6e5035c031fa
bolladaniele <bolladaniele@gmail.com>
2021-03-16 09:52:42 +0000

    PKTL-258

--- src/pages/ViewVideo.vue
+++ src/pages/ViewVideo.vue
@@ -256,7 +256,7 @@ export default {
       this.isVideoMenuDropped = !this.isVideoMenuDropped;
     },
     async startTimer() {
-      await delay(10000);
+      await delay(120000);
       this.isPopoverTTProgress = false;
     },
     async tipStreamer() {
@@ -309,7 +309,7 @@ export default {
         this.isPopoverClickTT = false;
       });
     });
-    await delay(900000);
+    await delay(10000);
     this.isPopoverClickTT = false;
   }
 };
