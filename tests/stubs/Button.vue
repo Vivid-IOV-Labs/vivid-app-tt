3c353734db54a671d95abc75b4af4346709dd27b
bolladaniele <bolladaniele@gmail.com>
2021-04-21 10:50:15 +0100

    BaseCheckButton test

---
+++ tests/stubs/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="callback">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    callback() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
---
+++ tests/unit/components/BaseCheckButton.spec.js
@@ -0,0 +1,92 @@
+import { shallowMount } from "@vue/test-utils";
+import BaseCheckButton from "@/components/BaseCheckButton";
+import Button from "../../stubs/Button";
+
+describe("BaseCheckButton", () => {
+  // SUGGESTION: write your tests here
+  it("should act as an input checkbox with array model", async () => {
+    const wrapper = shallowMount(BaseCheckButton, {
+      propsData: {
+        text: "Test",
+        name: "test",
+        value: []
+      },
+      stubs: {
+        "v-ons-button": Button
+      }
+    });
+    expect(wrapper.attributes().role).toBe("checkbox");
+    await wrapper.trigger("click");
+    expect(wrapper.emitted("input")).toBeTruthy();
+    expect(wrapper.emitted().input.flat(2).length).toBe(1);
+    expect(wrapper.emitted().input.flat(2)[0]).toBe("test");
+  });
+  it("should be selectable", async () => {
+    const value = ["test"];
+    const wrapper = shallowMount(BaseCheckButton, {
+      propsData: {
+        text: "Test",
+        name: "test",
+        selected: true,
+        value
+      },
+      stubs: {
+        "v-ons-button": Button
+      }
+    });
+    expect(wrapper.vm.selected).toBe(true);
+    expect(wrapper.attributes().checked).toBeTruthy();
+    expect(wrapper.classes("btn-check--selected")).toBe(true);
+  });
+  it("should be unselectable", async () => {
+    const value = ["test"];
+    const wrapper = shallowMount(BaseCheckButton, {
+      propsData: {
+        text: "Test",
+        name: "test",
+        value
+      },
+      stubs: {
+        "v-ons-button": Button
+      }
+    });
+    expect(wrapper.vm.selected).toBe(false);
+    expect(wrapper.attributes().checked).toBeFalsy();
+    expect(wrapper.classes("check-button--selected")).toBe(false);
+  });
+  it("should be unselectable", async () => {
+    const value = ["test"];
+    const wrapper = shallowMount(BaseCheckButton, {
+      propsData: {
+        text: "Test",
+        name: "test",
+        value
+      },
+      stubs: {
+        "v-ons-button": Button
+      }
+    });
+    expect(wrapper.vm.selected).toBe(false);
+    expect(wrapper.attributes().checked).toBeFalsy();
+    expect(wrapper.classes("check-button--selected")).toBe(false);
+  });
+  it("should limit selection", async () => {
+    const value = ["first", "second"];
+    const wrapper = shallowMount(BaseCheckButton, {
+      propsData: {
+        text: "Test",
+        name: "test",
+        limit: 2,
+        value
+      },
+      stubs: {
+        "v-ons-button": Button
+      }
+    });
+    await wrapper.trigger("click");
+    const emitted = wrapper.emitted().input.flat(2);
+    const { length } = emitted;
+    expect(length).toBe(2);
+    expect(emitted[length - 1]).toBe("test");
+  });
+});
